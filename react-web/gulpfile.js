e9705800f9bdec1dd974b1c9470e0b6b63a752af
vinny <vincentmarcos@hotmail.com>
2017-07-07 19:00:07 +1000

    errors added

--- react-web/gulpfile.js
+++ react-web/gulpfile.js
@@ -8,12 +8,14 @@ gulp.task( 'default', ['sass:watch'] )
 
 gulp.task('sass', function () {
   return gulp.src('./src/stylesheets/*.scss')
-  .pipe(sass())
+  .pipe(sass().on('error', sass.logError))
   .pipe(concat('custom.css')) // this is what was missing
   .pipe(gulp.dest('./src')); // output to theme root
   // .pipe(sass.sync().on('error', sass.logError))
+  console.log('asdf')
 });
 
 gulp.task('sass:watch', function () {
+  gulp.task('execute')
   gulp.watch('./src/stylesheets/*.scss', ['sass']);
 });
