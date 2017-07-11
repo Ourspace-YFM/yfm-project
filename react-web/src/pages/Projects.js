b97d1731617170ce60c9e31c17a42fc6b277753b
alfred.osorio <alfred.osorio@outlook.com>
2017-07-11 21:15:41 +1000

    Use mapping

--- react-web/src/pages/Projects.js
+++ react-web/src/pages/Projects.js
@@ -2,21 +2,82 @@ import React from 'react'
 
 // MUI Components
 import ExpandableCard from '../molecules/ExpandableCard'
-import SummaryItem from '../molecules/SummaryItem'
 import ProgressItem from '../molecules/ProgressItem'
 import BoldText from '../components/BoldText'
 import LightText from '../components/LightText'
-import Logo from '../components/Logo'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+const Data = {
+	"clients": [
+		{
+			"_id": "jjkadnfkan",
+			"client": "Coles",
+			"logo": "https://i.imgur.com/zgANrVM.jpg",
+			"projects": [
+				{
+					"name": "Johnny",
+					"type": "New Truck",
+					"status": "closed",
+					"urgent": false
+				},
+				{
+					"name": "Click and Collect",
+					"type": "Rebrand",
+					"status": "open",
+					"urgent": true
+				},
+				{
+					"name": "Maintainance & Repairs",
+					"type": "Repairs",
+					"status": "open",
+					"urgent": true
+				},
+				{
+					"name": "Visit new store for measure up",
+					"type": "inspect",
+					"status": "open",
+					"urgent": true
+				}
+			]
+		},
+		{
+			"_id": "ksdjbnkdfv",
+			"client": "Supercheap storage",
+			"logo": "logo.png",
+			"projects": [
+				{
+					"name": "SCS Perth & LGL Burwood",
+					"type": "New Truck",
+					"status": "closed",
+					"urgent": false
+				},
+				{
+					"name": "Nightshopper",
+					"type": "Rebrand",
+					"status": "open",
+					"urgent": true
+				}
+			]
+		}
+	]
+}
 
 const Projects = () => (
     <div>
         <h1>Projects</h1>
 
+          { 
+            Data.clients.map((item) => (
+              <p>
+                { item.client }
+                { console.log( item.client ) }
+              </p>
+            ))
+          }
+
         <div>
           <ExpandableCard 
-            subtitle="6 Projects"
-            logoSrc="https://i.imgur.com/zgANrVM.jpg"
+            subtitle={ (Data.clients[0].projects.length) + " Projects" }
+            logoSrc={ Data.clients[0].logo }
             logoAlt="Test Logo"
             logoClass="testClassName"
             children="Test" >
@@ -24,128 +85,27 @@ const Projects = () => (
             <br/>
 
             <div>
-              <ProgressItem title="00912211384742" subtitle="Johnny" completed="30" numerator="3" denominator="10">
+              <ProgressItem title={ Data.clients[0].projects[0].name } completed={30} numerator={3} denominator={10}>
             <br/>
             <br/>   
               <div>
                 <BoldText text="Type:" />  <br />
-                <LightText text="New Truck" />
+                <LightText text={ Data.clients[0].projects[0].type } />
               </div>
             <br/>
             <br/>        
               <div>
                 <BoldText text="Status:" />  <br />
-                <LightText text="Closed" />
+                <LightText text={ Data.clients[0].projects[0].status } />
               </div>
             <br/>
             <br/>   
- 
               </ProgressItem>            
             </div>
           </ExpandableCard>
         </div>
 
-        <div>
-          <ExpandableCard 
-            subtitle="6 Projects"
-            logoSrc="https://i.imgur.com/zgANrVM.jpg"
-            logoAlt="Test Logo"
-            logoClass="testClassName"
-            children="Test" >
-            <br/>
-            <br/>
-
-            <div>
-              <ProgressItem title="00912211384742" subtitle="Johnny" completed="30" numerator="3" denominator="10">
-            <br/>
-            <br/>   
-              <div>
-                <BoldText text="Type:" />  <br />
-                <LightText text="New Truck" />
-              </div>
-            <br/>
-            <br/>        
-              <div>
-                <BoldText text="Status:" />  <br />
-                <LightText text="Closed" />
-              </div>
-            <br/>
-            <br/>   
- 
-              </ProgressItem>            
-            </div>
-          </ExpandableCard>
-        </div>
-
-        <div>
-          <ExpandableCard 
-            subtitle="6 Projects"
-            logoSrc="https://i.imgur.com/zgANrVM.jpg"
-            logoAlt="Test Logo"
-            logoClass="testClassName"
-            children="Test" >
-            <br/>
-            <br/>
-
-            <div>
-              <ProgressItem title="00912211384742" subtitle="Johnny" completed="30" numerator="3" denominator="10">
-            <br/>
-            <br/>   
-              <div>
-                <BoldText text="Type:" />  <br />
-                <LightText text="New Truck" />
-              </div>
-            <br/>
-            <br/>        
-              <div>
-                <BoldText text="Status:" />  <br />
-                <LightText text="Closed" />
-              </div>
-            <br/>
-            <br/>   
- 
-              </ProgressItem>            
-            </div>
-          </ExpandableCard>
-        </div>
-
-        <div>
-          <ExpandableCard 
-            subtitle="6 Projects"
-            logoSrc="https://i.imgur.com/zgANrVM.jpg"
-            logoAlt="Test Logo"
-            logoClass="testClassName"
-            children="Test" >
-            <br/>
-            <br/>
-
-            <div>
-              <ProgressItem title="00912211384742" subtitle="Johnny" completed="30" numerator="3" denominator="10">
-            <br/>
-            <br/>   
-              <div>
-                <BoldText text="Type:" />  <br />
-                <LightText text="New Truck" />
-              </div>
-            <br/>
-            <br/>        
-              <div>
-                <BoldText text="Status:" />  <br />
-                <LightText text="Closed" />
-              </div>
-            <br/>
-            <br/>   
- 
-              </ProgressItem>            
-            </div>
-          </ExpandableCard>
-        </div>                        
-
-
-        
     </div>
-
-    
 )
 
 export default Projects
