f381e7a894be86a4d5f5ac0a76dfd03189b43869
Vincent Lloyd <vincent.lloyd@protonmail.com>
2017-07-12 16:46:31 +1000

    Create model files: booking, company, file, job, project, task

---
+++ api/models/job.js
@@ -0,0 +1,14 @@
+const mongoose = require('./init')
+    , Schema = mongoose.Schema
+
+const jobSchema = Schema({
+  parentProject: { type: Schema.Types.ObjectId, ref: 'Project' },
+  jobOwner: { type: Schema.Types.ObjectId, ref: 'Company' },
+  jobName: String,
+  jobStatus: String,
+  jobDescription: String
+})
+
+const Job = mongoose.model('Job', jobSchema)
+
+module.exports = Job
