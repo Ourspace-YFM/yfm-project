6ce9980094704e098fae2c816b7d6beda95c04a0
Ann Siapno <ann.n.siapno@gmail.com>
2017-07-14 11:17:13 +1000

    Map refreshed to original state

--- react-web/src/components/molecules/Map.js
+++ react-web/src/components/molecules/Map.js
@@ -3,7 +3,8 @@ import GoogleMapReact from 'google-map-react';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-export default class SimpleMap extends Component {
+export default class SimpleMap extends Component{
+
   static defaultProps = {
     center: {lat: 59.95, lng: 30.33},
     zoom: 11
@@ -11,17 +12,17 @@ export default class SimpleMap extends Component {
 
   render() {
     return (
-      <div style={{height: '200px', width: '200px',marginLeft: 'auto', marginRight: 'auto'}}> 
-      <GoogleMapReact
-        defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text={'Kreyser Avrora'}
-        />
-      </GoogleMapReact>
+      <div style={{height: '400px', width: '600px',marginLeft: 'auto', marginRight: 'auto'}}>
+        <GoogleMapReact
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Test'}
+          />
+        </GoogleMapReact>
       </div>
     );
   }
