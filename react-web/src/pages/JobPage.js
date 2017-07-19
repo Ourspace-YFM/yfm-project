9ae084277b36b8a045da0912394eb52943fd4dd3
alfred.osorio <alfred.osorio@outlook.com>
2017-07-19 21:59:16 +1000

    Complete Task and Job views

--- react-web/src/pages/JobPage.js
+++ react-web/src/pages/JobPage.js
@@ -5,6 +5,8 @@ import Logo from '../components/atoms/Logo'
 import LightText from '../components/atoms/LightText'
 import BoldText from '../components/atoms/BoldText'
 import Subtitle from '../components/atoms/Subtitle'
+import ExpandableCard from '../components/molecules/ExpandableCard'
+import LinkButton from '../components/atoms/LinkButton'
 
 export default function Job({
   data
@@ -17,12 +19,11 @@ export default function Job({
       {
         !!data ? (
           <div>
-          { JSON.stringify(data) }
           <h1>{ data.job.name }</h1>
           <Logo src={data.job.owner.logo} alt={data.job.owner.name + " Logo"} className="lrg"/>
           <div>
             <BoldText text="Project: "/>
-            <LightText text={data.job.parentProject.description}/>
+            <LightText text={data.job.owner.name}/>
           </div>
           <div className="jobDetails">
             <Subtitle text="Job Details"/>
@@ -43,8 +44,22 @@ export default function Job({
               <LightText/>
             </div>
 
-          </div>
+            { data.tasks.map((task) => (
+              <div className='tasks'>
+                <ExpandableCard
+                  title={ task.name }
+                  logoClass="logo-class"
+                  children={
+                    <div>
+                      <BoldText text="Status: " />
+                      <LightText text={ task.status } /> <br />
+                      <LinkButton label='View' buttonColor='#F06734' link={ `/tasks/${task._id}` }/>
+                    </div>                    
+                  } />
+              </div>              
+            ))}
 
+          </div>
           </div>
         ) : (
           'Loading Job'
--- react-web/src/pages/TaskPage.js
+++ react-web/src/pages/TaskPage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import LinkButton from '../components/atoms/LinkButton'
 import ExpandableCard from '../components/molecules/ExpandableCard'
-
-import * as dataSorter from '../api/dataSorter'
+import LightText from '../components/atoms/LightText'
+import BoldText from '../components/atoms/BoldText'
 
 export default function Task({
   data
 }) {
-    if(!!data) {
-      'data',data
+    if (!!data) {
+      console.log(data)
     }
 
   return (
@@ -16,22 +16,43 @@ export default function Task({
       {
         !!data ? (
           <div>
-            <p>{ data.task }</p>   
-            <p>{ data.task }</p>
-            <p>{ data.task }</p>
-            <div className='tasks'>
-              {
-                <ExpandableCard
-  								title={ data.task.name }
-  								logoClass="logo-class"
-  								children="Test" >
-  									<div>
-                      <LinkButton buttonColor='#F06734' link={ `/bookings/${data.bookings._id}` }/>
-  									</div>
-  							</ExpandableCard>               
+
+            <h2>{data.task.name}</h2> 
+
+            <div>
+              <BoldText text='Attachments:' /> <LightText text={ data.task.attachments } />
+            </div>
+
+            <div>
+              <BoldText text='Status:' /> <LightText text={ data.task.status } />
+            </div>
+
+            <div>
+              <BoldText text='Parent Job:' /> <LightText text={ data.task.parentJob.name } />
+            </div>
+
+            <br />
+
+              { /* (data.bookings) information to go here when bookings seeds have been added. */
+                !!data.bookings._id ? (
+                  <div className='tasks'>
+                    <ExpandableCard
+                      title="Bookings"
+                      logoClass="logo-class"
+                      children={
+                      /* If there are bookings, display a link button. If there aren't, display a message */
+                        <div>
+                          <LinkButton buttonColor='#F06734' link={ `/bookings/${data.bookings._id}` } />
+                        </div>
+                      } />
+                  </div>
+                ) : (
+                  <div>
+                    <LightText text='There are currently no assigned bookings for this task.' />
+                  </div>
+                )
               }
 
-            </div>
           </div>
         ) : (
           'Loading Tasks'
