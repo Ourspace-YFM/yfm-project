8817657589539cd54cb38e7920f1e193e314d932
Ann Siapno <ann.n.siapno@gmail.com>
2017-07-23 16:33:24 +1000

    create bookings page layout

--- react-web/src/pages/BookingsPage.js
+++ react-web/src/pages/BookingsPage.js
@@ -7,59 +7,19 @@ import BoldText from '../components/atoms/BoldText'
 import LightText from '../components/atoms/LightText'
 import * as dataSorter from '../api/dataSorter'
 
-const Projects = ({
-	projects
+const Bookings = ({
+	bookings
 
 }) => {
 	return (
 		<div>
 				<h1>Bookings</h1>
-				{!!projects ? (
+				{!!bookings ? (
 					<div>
-						{ dataSorter.clientsWithGroupedProjects(projects).map((client) => (
-							<ExpandableCard
-								logoSrc={ client.logo }
-								logoAlt="Test Logo"
-								logoClass="sml" >
-								{client.projects.map((project)=>(
-									<div key={ project._id }>
-										<div className="cardInfoContainer">
-											<ProgressItem
-												className="projectsPageProgressItem"
-												completed={ 35 }
-												numerator={ 3 }
-												denominator={ 10 }
-												link={ `/projects/${project._id}` }
-												buttonLabel="View">
-												<div className="progressItemInfo">
-													<BoldText text="Description" />
-													<br/><br/>
-													<LightText text={ project.description } />
-												</div>
-												<div className="progressItemInfo">
-													<BoldText text="Start Date" />
-													<br/><br/>
-													<LightText text={ project.startDate } />
-												</div>
-												<div className="progressItemInfo">
-													<BoldText text="End Date" />
-													<br/><br/>
-													<LightText text={ project.endDate } />
-												</div>
-												<div className="progressItemInfo">
-													<BoldText text="Type" />
-													<br/><br/>
-													<LightText text={ project.type } />
-												</div>
-											</ProgressItem>
-										</div>
-									</div>
-								))}
-							</ExpandableCard>
-						))}
+						"Loaded"
 					</div>
 					):(
-						'Loading Projects'
+						'Loading Bookings'
 					)
 				}
 
@@ -67,4 +27,4 @@ const Projects = ({
 	)
 }
 
-export default Projects
+export default Bookings
